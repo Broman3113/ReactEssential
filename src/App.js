fa9c33bf7963ab532c9febef4d1af58347682c21
Andrey Borbot <broman3113@gmail.com>
2020-12-09 17:21:03 +0200

    Modified Main Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const MyComponent = () => {
+  return <div>This is the Main page on React Essential!</div>
+}
+
+//Class Component
+class MyClassComponent extends React.Component{
+  render() {
+    return <div>To see lessons check corresponding Branches :)</div>
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <br/>
+          <MyComponent/>
+
+          <MyClassComponent/>
+
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
