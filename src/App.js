a3e678c21b9fd9f323b81bce307ddf683c78a1d9
Andrey Borbot <broman3113@gmail.com>
2020-12-09 17:00:51 +0200

    Completed lesson 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const MyComponent = () => {
+  return <div>(This is my component text)</div>
+}
+
+const MyProp = props => {
+  return <div>This is my ({props.text})</div>
+}
+
+const MyText = ({text = "Default text"}) => {
+  return <div>Text ({text})</div>
+}
+
+//Best option
+const MyTextTernar = props =>{
+  return <div>Best option text ({props.text || "Default text"})</div>
+}
+
+//Class Component
+class MyClassComponent extends React.Component{
+  render() {
+    return <div>Class text ({this.props.text || "Default class text"})</div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/*<MyComponent></MyComponent> //Warning that has empty tag but we can use it too*/}
+        <MyComponent/>
+
+        <MyProp text="Prop text 1"/>
+        <MyProp text="Prop text 2"/>
+
+        <MyText text="1"/>
+        <MyText/>
+
+        <MyTextTernar text="1"/>
+        <MyTextTernar/>
+
+        <MyClassComponent text="1"/>
+        <MyClassComponent/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
