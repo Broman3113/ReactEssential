fb15ea40ae39baa7ab096d3557615354684ca7e7
Andrey Borbot <broman3113@gmail.com>
2020-12-16 14:47:45 +0200

    Fixed Lesson-5

--- src/components/JSONForm.js
+++ src/components/JSONForm.js
@@ -4,26 +4,31 @@ export class JSONForm extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
+            value: '',
             first: '',
             second: '',
             third: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    handleChange(event){
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
+    handleInputChange(event){
+        const name = event.target.name;
+        const value = event.target.value;
         this.setState({
             [name]: value
         });
     }
 
+    handleChange(event){
+        this.setState({value: event.target.value})
+    }
+
     handleSubmit(event){
         event.preventDefault();
+        alert(`Hi ${this.state.first} ${this.state.second} ${this.state.third}`);
     }
 
     render() {
@@ -31,28 +36,28 @@ export class JSONForm extends React.Component{
             <form onSubmit={this.handleSubmit}>
                 <label>
                     <input
+                        type="text"
                         name="first"
-                        type="text"
+                        onChange={this.handleInputChange}
                         value={this.state.first}
-                        onChange={this.handleChange} />
-                </label>
-                <label>
+                    />
                     <input
+                        type="text"
                         name="second"
-                        type="text"
+                        onChange={this.handleInputChange}
                         value={this.state.second}
-                        onChange={this.handleChange} />
-                </label>
-                <label>
+                    />
                     <input
-                        name="third"
                         type="text"
+                        name="third"
+                        onChange={this.handleInputChange}
                         value={this.state.third}
-                        onChange={this.handleChange} />
-                    <p>{this.state.value}</p>
+                    />
                 </label>
-                <input type="submit" value="Submit" onClick={this.handleSubmit}/>
-
+                <input type="submit" value="Send"/>
+                <div>
+                        {this.state.first} <br/> {this.state.second} <br/> {this.state.third}
+                </div>
 
             </form>
         );
